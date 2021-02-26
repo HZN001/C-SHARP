cc52396540290f1c0a24a9d037258804f0303af4
Heitor Silva <heitorsilvanascimento@hotmail.com>
2021-02-26 14:18:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \n diz para o computador quebrar uma linha
+           
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Meu nome é Heitor");
+            //Write o ponto Line, tem a mesma função, pórem fica mais bonito
+
+
+            Console.ReadLine();
+        
+        }
+    }
+}
